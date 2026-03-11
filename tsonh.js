292278c3333950ee817d91d451d700da9fb07c57
azula2580 <azula2580@gmail.com>
2026-03-11 19:24:48 +0800

    Update tsonh.js

--- tsonh.js
+++ tsonh.js
@@ -1,3 +1,9 @@
+{
+  "imports": {
+    "three": "https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js",
+    "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/"
+  }
+}
 import * as THREE from "three";
 export default class Tsonh {
     constructor({
@@ -41,4 +47,5 @@ export default class Tsonh {
         frameRight.position.x = this.urgun/2;
         this.group.add(frameRight);
     }
+
 }
